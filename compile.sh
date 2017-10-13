c974542d01cadca7396e9e06c81bb5f975621160
mewild <michael@wildapplications.com>
2017-10-13 13:47:27 +0100

    latest updates

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-docker build -t blueapp/proxy:0.0.2 . &&
+docker build -t wildapplications/proxy:latest . &&
 kubectl scale --replicas=0 deployment deployment --namespace=proxy &&
 kubectl scale --replicas=2 deployment deployment --namespace=proxy
